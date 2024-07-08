13e822fe587d951bddc1f9634646418f28965ec2
Audrey <audreyalcantara@Audreys-MBP.lan>
2024-07-08 05:13:59 -0400

    test: add test to validate current functionailty of code for aged brie

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,11 +7,35 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Fact]
-    public void foo()
+    public void UpdateQuality_OfNormalItem_WithZeroQuality_Returns_NeverNegative()
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var name = "foo";
+        var quality = 0;
+
+        IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = 0, Quality = quality } };
         GildedRose app = new GildedRose(Items);
         app.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+
+        Assert.Equal(name, Items[0].Name);
+        Assert.Equal(quality, Items[0].Quality);
     }
+
+    [Fact]
+    public void UpdateQuality_OfAgedBrie_Returns_AppropriateUpdate()
+    {
+        var name = "Aged Brie";
+        var quality = 5;
+        var sellIn = 5;
+        var expectedQuality = 6;
+        var expectedSellIn = 4;
+
+        IList<Item> Items = new List<Item> { new Item { Name = name, Quality = quality, SellIn = sellIn } };
+        GildedRose app = new GildedRose(Items);
+
+        app.UpdateQuality();
+
+        Assert.Equal(expectedQuality, Items[0].Quality);
+        Assert.Equal(expectedSellIn, Items[0].SellIn);
+    }
+
 }
